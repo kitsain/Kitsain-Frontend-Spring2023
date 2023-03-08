5d481177ff1697482a7835bdd29d17605ae6380d
Ella Elomaa <98178412+ellaelomaa@users.noreply.github.com>
2023-03-08 12:41:47 +0200

    Deleted extra files

--- lib/realm/appServices.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:realm/realm.dart';
-
-class AppServices {}
--- lib/realm/realmServices.dart
+++
@@ -1,3 +0,0 @@
-import 'package:realm/realm.dart';
-import 'package:flutter/material.dart';
-import 'package:kitsain_frontend_spring2023/realm/item.dart';
