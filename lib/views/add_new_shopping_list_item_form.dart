070a33485f7a73dad7a65c9853f86b6034beaf8d
unknown <seamus.svahn@tuni.fi>
2023-03-26 12:40:06 +0300

    Add shopping list item form. Copied add item form without date-fields.

---
+++ lib/views/add_new_shopping_list_item_form.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/material.dart';
+import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
+
+const List<String> categories = <String>['Meat', 'Seafood', 'Fruit', 'Vegetables',
+  'Frozen', 'Drinks', 'Bread', 'Sweets',
+  'Dairy', 'Ready meals',
+  'Dry & canned goods', 'Other'];
+
+class NewShoppingListItemForm extends StatefulWidget {
+  const NewShoppingListItemForm({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _NewItemFormState createState() => _NewItemFormState();
+}
+
+@override
+class _NewItemFormState extends State<NewShoppingListItemForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _barcodeField = TextEditingController();
+  var _itemName = TextEditingController();
+  bool click = true;
+  String dropdownValue = categories.first;
+
+  void _discardChangesDialog() {
+    if(_itemName.text.isEmpty && _barcodeField.text.isEmpty) {
+      Navigator.pop(context);
+    } else {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+            content: const Text('Discard changes?'),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('CANCEL'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              TextButton(
+                child: const Text('DISCARD'),
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          )
+      );
+    }
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: ListView(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          padding: const EdgeInsets.all(8),
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.05,
+                  child: FloatingActionButton(
+                    child: Icon(Icons.close),
+                    onPressed: () => _discardChangesDialog(), //Close sheet
+                  ),
+                )
+              ],
+            ),
+            Text(
+              'ADD TO\n SHOPPING LIST',
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox( height: MediaQuery.of(context).size.height * 0.03),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.07,
+                  child: ElevatedButton.icon(
+                    onPressed: () async {
+                      var res = await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SimpleBarcodeScannerPage(),
+                          ));
+                      setState(() {
+                        if (res is String) {
+                          _barcodeField.text = res;
+                        }
+                      });
+                      //Res will be the EAN-code
+                      //Here add OFF-api call and populate item name and category
+                      //fields if the product was found
+                      //_itemName.text =
+                      //_itemCat.text =
+                    },
+                    icon: Icon(Icons.camera_alt),
+                    label: const Text('SCAN ITEM'),
+                  ),
+                ),
+                SizedBox( height: MediaQuery.of(context).size.height * 0.03),
+                SizedBox(
+                  width: 150,
+                  child: TextFormField(
+                    controller: _barcodeField,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Barcode',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox( height: MediaQuery.of(context).size.height * 0.03),
+            SizedBox(
+              child: TextFormField(
+                controller: _itemName,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Item name',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter item name";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            SizedBox( height: MediaQuery.of(context).size.height * 0.03),
+            //Category-field
+            SizedBox(
+              child: DropdownButtonFormField<String>(
+                value: dropdownValue,
+                onChanged: (String? value) {
+                  setState(() {
+                    dropdownValue = value!;
+                  });
+                },
+                items: categories.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ),
+            SizedBox( height: MediaQuery.of(context).size.height * 0.01),
+            SizedBox(
+              child: TextButton.icon(
+                onPressed: () {
+                  setState(() {
+                    click = !click;
+                  });
+                },
+                icon: Icon((click == false) ? Icons.favorite : Icons.favorite_border),
+                label: Text('Mark as an "everyday" item'),
+              ),
+            ),
+            SizedBox( height: MediaQuery.of(context).size.height * 0.03),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.07,
+                  child: ElevatedButton(
+                    onPressed: () => _discardChangesDialog(),
+                    child: Text('CANCEL'),
+                  ),
+                ),
+
+                SizedBox( width: MediaQuery.of(context).size.width * 0.05),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.07,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if(_formKey.currentState!.validate()) {
+                        print("OK");
+                      }
+                    },
+                    child: Text('ADD ITEM'),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        )
+    );
+  }
+}
+
