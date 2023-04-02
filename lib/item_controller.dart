2c7bf8166a0174f86968bc7abed8a9f07130876a
unknown <seamus.svahn@tuni.fi>
2023-04-02 13:02:05 +0300

    Test data for history tab.

--- lib/item_controller.dart
+++ lib/item_controller.dart
@@ -1,8 +1,12 @@
+import 'dart:math';
+
 import 'package:get/get.dart';
 
 class ItemController extends GetxController {
   RxList<String> shoppingBagList = <String>[].obs;
   RxList<String> pantryList = <String>[].obs;
+  RxList<String> expiredList = <String>[].obs;
+  RxList<String> usedList = <String>[].obs;
 
   addData() {
     shoppingBagList.add('shopping list 1');
@@ -14,5 +18,27 @@ class ItemController extends GetxController {
     pantryList.add('pantry 3');
     pantryList.add('pantry 4');
     pantryList.add('pantry 5');
+
+    expiredList.add('expired item 1');
+    expiredList.add('expired item 2');
+    expiredList.add('expired item 3');
+    expiredList.add('expired item 4');
+    expiredList.add('expired item 5');
+    expiredList.add('expired item 6');
+    expiredList.add('expired item 7');
+    expiredList.add('expired item 8');
+    expiredList.add('expired item 9');
+    expiredList.add('expired item 10');
+
+    usedList.add('used item 1');
+    usedList.add('used item 2');
+    usedList.add('used item 3');
+    usedList.add('used item 4');
+    usedList.add('used item 5');
+    usedList.add('used item 6');
+    usedList.add('used item 7');
+    usedList.add('used item 8');
+    usedList.add('used item 9');
+    usedList.add('used item 10');
   }
 }
