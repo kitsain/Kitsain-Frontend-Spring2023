523e35babfd136ed956305e3476544e919669cf4
unknown <seamus.svahn@tuni.fi>
2023-04-03 17:28:47 +0300

    All widgets inside one listview.builder. Show graph only when 'Expired items' is toggled. Draggable items.

--- lib/views/main_menu_pages/used_and_expired.dart
+++ lib/views/main_menu_pages/used_and_expired.dart
@@ -40,120 +40,107 @@ class _UsedAndExpiredState extends State<UsedAndExpired> {
     return Scaffold(
           body: Column(
             children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.03,
-                  child: Text(
-                    'MONTH > $month $year',
-                    textAlign: TextAlign.right,
-                    style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-              Stack(
-                  children: <Widget>[
-                    Container(
-                      width: 230,
-                      height: 180,
-                      //color: Colors.black,
-                    ),
-                    Positioned(
-                      left: 20,
-                      right: 20,
-                      child: Container(
-                        width: 200,
-                        height: 150,
-                        color: Colors.red,
-                      ),
-                    ),
-                    Positioned(
-                      top: 120,
-                      left: 170,
-                      right: 10,
-                      bottom: 10,
-
-                      child: Container(
-                        alignment: Alignment.center,
-                        width: 50,
-                        height: 50,
-                        color: Colors.blue,
-                        child: Text('25 %'),
-                      ),
-                    ),
-                  ],
-                ),
-              SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-              Center(
-                child: ToggleButtons(
-                    direction: Axis.horizontal,
-                    onPressed: (int index) {
-                      setState(() {
-                        // The button that is tapped is set to true, and the others to false.
-                        for (int i = 0; i < _selectedTabs.length; i++) {
-                          _selectedTabs[i] = i == index;
-                        }
-                      });
-                    },
-                    borderRadius: const BorderRadius.all(Radius.circular(8)),
-                    selectedBorderColor: Colors.red[700],
-                    selectedColor: Colors.white,
-                    fillColor: Colors.red[200],
-                    color: Colors.red[400],
-                    constraints: const BoxConstraints(
-                      minHeight: 40.0,
-                      minWidth: 80.0,
-                    ),
-                    children: tabs,
-                    isSelected: _selectedTabs
-                ),
-              ),
               Flexible(
                 child: ListView.builder(
                   scrollDirection: Axis.vertical,
                   shrinkWrap: true,
-                  itemCount: _selectedTabs[0] ? StateController.expiredList.length : StateController.usedList.length,
+                  //On the first iteration (index == 0) create the top part of the list. On next iterations create the item cards.
+                  //Add 1 to list lengths in order to not lose one item because of this.
+                  itemCount: _selectedTabs[0] ? StateController.expiredList.length+1 : StateController.usedList.length+1,
                   itemBuilder: (context, index) {
-                  return ListTile(
-                  title: Text(_selectedTabs[0] ? StateController.expiredList[index] : StateController.usedList[index]),
-                    );
+                  return index == 0 ? Column(
+                      children: [
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.03,
+                            child: Text(
+                              'MONTH > $month $year',
+                              textAlign: TextAlign.right,
+                              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                        Center(
+                          child: ToggleButtons(
+                              direction: Axis.horizontal,
+                              onPressed: (int index) {
+                                setState(() {
+                                  for (int i = 0; i < _selectedTabs.length; i++) {
+                                    _selectedTabs[i] = i == index;
+                                  }
+                                });
+                              },
+                              borderRadius: const BorderRadius.all(Radius.circular(8)),
+                              constraints: const BoxConstraints(
+                                minHeight: 40.0,
+                                minWidth: 80.0,
+                              ),
+                              children: tabs,
+                              isSelected: _selectedTabs
+                          ),
+                        ),
+                        SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+                        if(_selectedTabs[0])
+                          Stack(
+                            children: <Widget>[
+                              Container(
+                                width: 230,
+                                height: 180,
+                              ),
+                              Positioned(
+                                left: 20,
+                                right: 20,
+                                child: Container(
+                                  width: 200,
+                                  height: 150,
+                                  color: Colors.red,
+                                  alignment: Alignment.center,
+                                  child: Text('Here the visual of wasted'),
+                                ),
+                              ),
+                              Positioned(
+                                top: 120,
+                                left: 170,
+                                right: 10,
+                                bottom: 10,
+                                child: Container(
+                                  alignment: Alignment.center,
+                                  width: 50,
+                                  height: 50,
+                                  color: Colors.blue,
+                                  child: Text('25 %'),
+                                ),
+                              ),
+                            ],
+                          ),
+                      ],
+                    ) : Draggable<String>(
+                    data: _selectedTabs[0] ? StateController.expiredList[index-1] : StateController.usedList[index-1],
+                    onDragCompleted: () {
+                      print('drag complete');
+                      //StateController.pantryList.removeAt(index);
+                    },
+                    feedback: Material(
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.75,
+                        height: MediaQuery.of(context).size.height * 0.10,
+                        child: ListTile(
+                          tileColor: Colors.lightGreen,
+                          title: Text('${_selectedTabs[0] ? StateController.expiredList[index-1] : StateController.usedList[index-1]}'),
+                        ),
+                      ),
+                    ),
+                    child: ListTile(
+                      title: Text('${_selectedTabs[0] ? StateController.expiredList[index-1] : StateController.usedList[index-1]}'),
+                    ),
+                  );
                   }
                 ),
               )
-              ],
-              )
-              );
+            ],
+          )
+    );
   }
 }
-
-
-/*children: [
-          DragTarget<String>(
-            onAccept: (data) => _receiveItem(data),
-            builder: (context, candidateData, rejectedData) {
-              return Container(
-                width: MediaQuery.of(context).size.width * 0.50,
-                height: MediaQuery.of(context).size.height * 0.20,
-                color: Colors.lightGreen,
-                alignment: Alignment.center,
-                child: Text(_placeholderDataModel),
-              );
-            },
-          ),
-        ],*/
-
-/*
-
-            Align(
-              alignment: Alignment.centerLeft,
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height * 0.03,
-                child: Text(
-                  'EXPIRED ITEMS',
-                  textAlign: TextAlign.right,
-                  style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
- */
