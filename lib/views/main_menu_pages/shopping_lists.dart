76bad7edfc7cbc33ace0af686c3e73877ad66d68
Johanna Viitanen <johanna.m.viitanen@tuni.fi>
2023-04-15 19:49:45 +0300

    Removed duplicate attribute that resulted from a merge

--- lib/views/main_menu_pages/shopping_lists.dart
+++ lib/views/main_menu_pages/shopping_lists.dart
@@ -5,7 +5,6 @@ import 'package:kitsain_frontend_spring2023/assets/top_bar.dart';
 import 'package:kitsain_frontend_spring2023/LoginController.dart';
 import 'package:kitsain_frontend_spring2023/controller/task_controller.dart';
 import 'package:kitsain_frontend_spring2023/controller/tasklist_controller.dart';
-import 'package:kitsain_frontend_spring2023/item_controller.dart';
 import 'package:kitsain_frontend_spring2023/views/add_new_shopping_list_form.dart';
 import 'package:kitsain_frontend_spring2023/views/homepage2.dart';
 import 'package:kitsain_frontend_spring2023/views/main_menu_pages/user_shopping_list.dart';
@@ -41,10 +40,6 @@ class _ShoppingListsState extends State<ShoppingLists> {
         );
       },
     );
-  }
-
-  _openShoppingList(int index) {
-    widget.setActiveShoppingListIndex(index);
   }
 
   _receiveItem(int index, String data) {
@@ -171,8 +166,6 @@ class _ShoppingListsState extends State<ShoppingLists> {
                                   context,
                                   MaterialPageRoute(
                                       builder: ((context) => UserShoppingList(
-                                            taskListId:
-                                                '${taskListController.taskLists.value?.items?[index].id}',
                                             taskListIndex: index,
                                             taskListId: '${taskListController.taskLists.value?.items?[index].id}',
                                             taskListName:
