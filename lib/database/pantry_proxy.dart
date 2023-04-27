3ee499a4480a1fcad7a3f08aa0aeac374be697cb
Ella Elomaa <98178412+ellaelomaa@users.noreply.github.com>
2023-04-27 18:50:31 +0300

    Sorting order fixed, icons changed in pantry

--- lib/database/pantry_proxy.dart
+++ lib/database/pantry_proxy.dart
@@ -40,16 +40,13 @@ class PantryProxy with ChangeNotifier {
     var all = getItems();
     late RealmResults<Item> result;
     if (sortBy == "az") {
-      result = all.query(
-          "location = \$0 SORT(hasExpiryDate DESC, name ASC)", ["Pantry"]);
+      result = all.query("location = \$0 SORT(name ASC)", ["Pantry"]);
     } else if (sortBy == "expdate") {
       result = all.query(
           "location = \$0 SORT(hasExpiryDate DESC, expiryDate ASC)",
           ["Pantry"]);
     } else if (sortBy == "addedlast") {
-      result = all.query(
-          "location = \$0 SORT(hasExpiryDate DESC, addedDate DESC)",
-          ["Pantry"]);
+      result = all.query("location = \$0 SORT(addedDate DESC)", ["Pantry"]);
     }
     return result;
   }
