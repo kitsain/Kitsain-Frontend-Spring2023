3ade4b9444022db7e8552dc06ee9e9a60ed0b878
Johanna Viitanen <johanna.m.viitanen@tuni.fi>
2023-04-27 18:23:21 +0300

    Fixed top bar icons overlapping with status bar on some screens, and made the layout match figma sketches better

--- lib/assets/top_bar.dart
+++ lib/assets/top_bar.dart
@@ -42,65 +42,81 @@ class _TopBarState extends State<TopBar> {
       preferredSize: widget.preferredSize,
       child: Container(
         height: MediaQuery.of(context).size.height * 0.2,
-        //color: Colors.green,
-        padding: const EdgeInsets.only(left: 15, top: 25, bottom: 1, right: 15),
+        padding: const EdgeInsets.only(left: 15, top: 0, bottom: 1, right: 15),
         decoration: BoxDecoration(
           image: DecorationImage(
-            //image: AssetImage("assets/images/pantry_banner_2.jpg"),
-            //image: AssetImage("assets/images/ue_banner_darker_3.png"),
             image: AssetImage(widget.backgroundImageName),
             fit: BoxFit.cover,
             alignment: Alignment.bottomCenter,
           ),
         ),
-        child: Row(
+        child: Column(
           children: [
-            Container(
-              width: MediaQuery.of(context).size.width * 0.64,
-              child: Text(
-                ' ${widget.title} ${'\u200e'}',
-                style: TextStyle(
-                    fontSize: 32,
-                    color: Colors.white,
-                    backgroundColor: widget.titleBackgroundColor,
-                ),
-              ),
-            ),
-            const Spacer(),
-            Column(
-              children: [
-                Row(
-                  children: [
-                    IconButton(
-                      visualDensity: topIconsDensity,
-                      padding: EdgeInsets.zero,
-                      onPressed: () => widget.helpFunction(),
-                      icon: const Icon(Icons.help_outline),
-                    ),
-                    IconButton(
-                      visualDensity: topIconsDensity,
-                      padding: EdgeInsets.zero,
-                      onPressed: () => _openSettings(),
-                      icon: const Icon(Icons.settings),
-                    ),
-                    IconButton(
-                      visualDensity: topIconsDensity,
-                      padding: EdgeInsets.zero,
-                      onPressed: () => _openAccountSettings(),
-                      icon: const Icon(Icons.account_circle),
-                    ),
-                  ],
-                ),
-                if (widget.addFunction != null)
+            SizedBox(height: MediaQuery.of(context).viewPadding.top),
+            IntrinsicHeight(
+              child: Row(
+                children: [
                   Container(
-                    height: 48,
-                    width: 48,
-                    child: FloatingActionButton(
-                      onPressed: () => widget.addFunction!(),
-                      child: Icon(widget.addIcon),
+                    alignment: Alignment.bottomLeft,
+                    width: MediaQuery.of(context).size.width * 0.64,
+                    child: Text(
+                      ' ${widget.title} ${'\u200e'}',
+                      style: TextStyle(
+                        fontSize: 32,
+                        color: Colors.white,
+                        backgroundColor: widget.titleBackgroundColor,
+                      ),
                     ),
                   ),
-              ],
+                  const Spacer(),
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          IconButton(
+                            visualDensity: topIconsDensity,
+                            padding: EdgeInsets.zero,
+                            onPressed: () => widget.helpFunction(),
+                            icon: const Icon(
+                              Icons.help_outline,
+                              color: Colors.white,
+                            ),
+                          ),
+                          IconButton(
+                            visualDensity: topIconsDensity,
+                            padding: EdgeInsets.zero,
+                            onPressed: () => _openSettings(),
+                            icon: const Icon(
+                              Icons.settings,
+                              color: Colors.white,
+                            ),
+                          ),
+                          IconButton(
+                            visualDensity: topIconsDensity,
+                            padding: EdgeInsets.zero,
+                            onPressed: () => _openAccountSettings(),
+                            icon: const Icon(
+                              Icons.account_circle,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      if (widget.addFunction != null)
+                        Container(
+                          height: 44,
+                          width: 44,
+                          child: FloatingActionButton(
+                            onPressed: () => widget.addFunction!(),
+                            child: Icon(widget.addIcon),
+                          ),
+                        ),
+                      if (widget.addFunction == null)
+                        const SizedBox(height: 44, width: 44),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
