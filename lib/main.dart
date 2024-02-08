9b89d395f37604750e524a8d5740f6cf5fbc9d0a
Joona Kostamo <joona.kostamo@outlook.com>
2024-02-08 16:41:15 +0200

    login fix

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,14 @@ import 'package:flutter_gen/gen_l10n/app-localizations.dart';
 import 'package:kitsain_frontend_spring2023/views/homepage2.dart';
 import 'package:kitsain_frontend_spring2023/views/main_menu_pages/shopping_list_navigation.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MaterialApp(home: MyApp()));
 }
 
