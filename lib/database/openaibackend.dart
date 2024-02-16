4846cc79b75e3ffd126d3ad50052f5a3b0b8dcc3
Tuupertunut <tuupertunut@outlook.com>
2024-02-16 16:29:08 +0200

    Package generated recipes in Recipe objects

--- lib/database/openaibackend.dart
+++ lib/database/openaibackend.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
+import 'package:kitsain_frontend_spring2023/database/item.dart';
 import 'package:http/http.dart' as http;
 
-Future<String?> generateRecipe(String ingredients, String recipe_type,
+Future<Recipe> generateRecipe(String ingredients, String recipe_type,
     String exp_soon, String supplies, String pantry_only) async {
   var url = Uri.https(
       'kitsain-build-ohtuprojekti-staging.apps.ocp-test-0.k8s.it.helsinki.fi',
@@ -15,11 +16,18 @@ Future<String?> generateRecipe(String ingredients, String recipe_type,
     'pantry_only': pantry_only
   });
   var response = await http.post(url, headers: headers, body: requestBody);
+
   print(
       "${ingredients}, ${recipe_type}, ${exp_soon}, ${supplies}, ${pantry_only}");
 
   print('Response status: ${response.statusCode}');
   print('Response body: ${response.body}');
 
-  return response.body;
+  var responseMap = json.decode(response.body);
+
+  /* The Recipe class only has these few fields so we have to hack the recipe data into those existing fields.
+   * Change this when Recipe class gets more complete. */
+  return Recipe(responseMap["recipe_name"], responseMap["recipe_name"],
+      details: json
+          .encode([responseMap["ingredients"], responseMap["instructions"]]));
 }
