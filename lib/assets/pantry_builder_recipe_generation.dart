6e0cf85789f0688f2b33d10bfb2247e93fa4fa05
erz64 <eeroranta111@gmail.com>
2024-02-26 16:27:29 +0200

    added list for expiring ingredients and the rest and modified ui

--- lib/assets/pantry_builder_recipe_generation.dart
+++ lib/assets/pantry_builder_recipe_generation.dart
@@ -1,43 +1,123 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:kitsain_frontend_spring2023/assets/item_card.dart';
 import 'package:kitsain_frontend_spring2023/database/item.dart';
 import 'package:realm/realm.dart';
 
-// In this file we build the list of item cards, either in the pantry
-// or in the history tab
-
 class PantryBuilder extends StatefulWidget {
   const PantryBuilder({
-    super.key,
+    Key? key,
     required this.items,
     required this.sortMethod,
     required this.onSelectedItemsChanged,
-  });
+  }) : super(key: key);
+
   final RealmResults<Item> items;
   final String sortMethod;
   final Function(List<String>) onSelectedItemsChanged;
+
 
   @override
   State<PantryBuilder> createState() => _PantryBuilderState();
 }
 
 class _PantryBuilderState extends State<PantryBuilder> {
-  late List<bool> isSelected;
+  late List<bool> isSelectedAll;
+  late List<bool> isSelectedNotExpiring;
+  late List<bool> isSelectedExpiring;
+  late List expiringItems = getExpiringItems();
+  late List notExpiringItems = getNotExpiringItems();
+  late DateFormat formatter = DateFormat('yyyy-dd-MM');
 
   @override
   void initState() {
     super.initState();
-    isSelected = List.generate(widget.items.length, (index) => false);
+    isSelectedAll = List.generate(widget.items.length, (index) => false);
+    isSelectedNotExpiring = List.generate(notExpiringItems.length, (index) => false);
+    isSelectedExpiring = List.generate(expiringItems.length, (index) => false);
   }
 
   List<String> getSelectedItems() {
-    List<String> selectedItems = [];
-    for (int i = 0; i < widget.items.length; i++) {
-      if (isSelected[i]) {
-        selectedItems.add(widget.items[i].name);
+    final selectedItems = <String>[];
+    for (int i = 0; i < notExpiringItems.length; i++) {
+      if (isSelectedNotExpiring[i]) {
+        selectedItems.add(notExpiringItems[i].name);
+      }
+    }
+    for (int i = 0; i < expiringItems.length; i++) {
+      if (isSelectedExpiring[i]) {
+        selectedItems.add(expiringItems[i].name);
       }
     }
     return selectedItems;
+  }
+
+  List getExpiringItems() {
+    final DateTime currentDate = DateTime.now();
+    final expiringItems = [];
+
+    for (var item in widget.items) {
+      if (item.expiryDate == null) {
+        continue;
+      }
+
+      if (item.expiryDate!.difference(currentDate).inDays <= 3){
+        expiringItems.add(item);
+      }
+    }
+    return expiringItems;
+  }
+
+
+  List getNotExpiringItems() {
+    final DateTime currentDate = DateTime.now();
+    final notExpiringItems = [];
+
+    for (var item in widget.items) {
+      if (item.expiryDate == null) {
+        notExpiringItems.add(item);
+        continue;
+      }
+
+      if (item.expiryDate!.difference(currentDate).inDays >= 3){
+        notExpiringItems.add(item);
+      }
+    }
+    return notExpiringItems;
+  }
+
+  void toggleSelectNotExpiring(bool select) {
+    setState(() {
+      isSelectedNotExpiring = List<bool>.filled(notExpiringItems.length, select);
+      widget.onSelectedItemsChanged(getSelectedItems());
+    });
+  }
+
+  void toggleItemSelectionNotExpiring(int index) {
+    setState(() {
+      isSelectedNotExpiring[index] = !isSelectedNotExpiring[index];
+      widget.onSelectedItemsChanged(getSelectedItems());
+      
+    });
+  }
+
+    void toggleSelectExpiring(bool select) {
+    setState(() {
+      isSelectedExpiring = List<bool>.filled(expiringItems.length, select);
+      widget.onSelectedItemsChanged(getSelectedItems());
+    });
+  }
+
+  void toggleItemSelectionExpiring(int index) {
+    setState(() {
+      isSelectedExpiring[index] = !isSelectedExpiring[index];
+      widget.onSelectedItemsChanged(getSelectedItems());
+    });
+  }
+
+  void toggleSelectAll(bool select) {
+    toggleSelectExpiring(select);
+    toggleSelectNotExpiring(select);
   }
 
   @override
@@ -45,64 +125,84 @@ class _PantryBuilderState extends State<PantryBuilder> {
     return SingleChildScrollView(
       child: Column(
         children: [
+          SizedBox(height: 30,),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    for (int i = 0; i < isSelected.length; i++) {
-                      isSelected[i] = true;
-                    }
-                    widget.onSelectedItemsChanged(getSelectedItems());
-                  });
-                },
-                child: Text('Select all'),
+                onPressed: () => toggleSelectAll(true),
+                child: const Text('Select all'),
               ),
               ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    for (int i = 0; i < isSelected.length; i++) {
-                      isSelected[i] = false;
-                    }
-                    widget.onSelectedItemsChanged(getSelectedItems());
-                  });
-                },
-                child: Text('Deselect all'),
+                onPressed: () => toggleSelectAll(false),
+                child: const Text('Deselect all'),
               ),
             ],
           ),
+          SizedBox(height: 30,),
+          Text("Expiring ingredients"),
+          SizedBox(height: 20,),
           Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Wrap(
-              spacing: 8.0,
+            padding: const EdgeInsets.all(8.0),
+            child: Align(alignment: Alignment.topLeft, child: Wrap(
+              alignment: WrapAlignment.start,
+              direction: Axis.vertical,
+              spacing: 1.0,
               runSpacing: 8.0,
               children: List.generate(
-                widget.items.length,
+                expiringItems.length,
                 (index) => GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      isSelected[index] = !isSelected[index];
-                      widget.onSelectedItemsChanged(getSelectedItems());
-                    });
-                  },
+                  onTap: () => toggleItemSelectionExpiring(index),
                   child: Container(
-                    padding: EdgeInsets.all(8.0),
+                    padding: const EdgeInsets.all(8.0),
                     decoration: BoxDecoration(
-                      color: isSelected[index]
-                          ? Color.fromARGB(255, 78, 117, 88)
+                      color: isSelectedExpiring[index]
+                          ? const Color.fromARGB(255, 78, 117, 88)
                           : null,
                       borderRadius: BorderRadius.circular(8.0),
                     ),
                     child: Text(
-                      widget.items[index].name,
-                      style: TextStyle(fontSize: 12.0),
+                      (expiringItems[index].name + " " + formatter.format(expiringItems[index].expiryDate)),
+                      
+                      style: const TextStyle(fontSize: 12.0),
                     ),
                   ),
                 ),
               ),
             ),
+            )
           ),
+          Text("Rest of ingredients"),
+          SizedBox(height: 20,),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Align(alignment: Alignment.topLeft, child:Wrap(
+              direction: Axis.vertical,
+              spacing: 1.0,
+              runSpacing: 8.0,
+              children: List.generate(
+                notExpiringItems.length,
+                (index) => GestureDetector(
+                  onTap: () => toggleItemSelectionNotExpiring(index),
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: isSelectedNotExpiring[index]
+                          ? const Color.fromARGB(255, 78, 117, 88)
+                          : null,
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: Text(
+                      notExpiringItems[index].name,
+                      style: const TextStyle(fontSize: 12.0),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            )
+          ),
+
         ],
       ),
     );
