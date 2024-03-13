9f2b790bb31dbc6d5b819b0c54c0bde1b48b9a4e
erz64 <eeroranta111@gmail.com>
2024-03-13 14:16:35 +0200

    added docstrings

--- lib/database/openaibackend.dart
+++ lib/database/openaibackend.dart
@@ -3,51 +3,62 @@ import 'package:kitsain_frontend_spring2023/database/item.dart';
 import 'package:http/http.dart' as http;
 import 'package:realm/realm.dart';
 
-Future<Recipe> generateRecipe(List<String> ingredients, String recipe_type,
-    List<String> exp_soon, List<String> supplies, bool pantry_only, String language) async {
+/// Generates a recipe using the parameters 
+/// [ingredients] (list of ingredients),
+/// [recipeType] (the type of recipe (eg. vegan)),
+/// [expSoon] (list of must have items in the recipe)
+/// [supplies] (list of kitchen supplies available for use in the recipe),
+/// [pantryOnly] (boolean value whether the recipe only uses items from the pantry or adds new ingredients)
+/// [language] (in which language is the recipe generated in)
+/// Returns a Recipe object with the generated recipe from ChatGPT
+Future<Recipe> generateRecipe(List<String> ingredients, String recipeType,
+    List<String> expSoon, List<String> supplies, bool pantryOnly, String language) async {
     var url = Uri.https(
       'kitsain-backend-test-ohtuprojekti-staging.apps.ocp-test-0.k8s.it.helsinki.fi',
       '/generate'); 
   var headers = {"Content-Type": "application/json"};
   var requestBody = json.encode({
     'ingredients': ingredients,
-    'recipe_type': recipe_type,
-    'exp_soon': exp_soon,
+    'recipeType': recipeType,
+    'expSoon': expSoon,
     'supplies': supplies,
-    'pantry_only': pantry_only,
+    'pantryOnly': pantryOnly,
     'language': language
   });
   print(
-      "${ingredients}, ${recipe_type}, ${exp_soon}, ${supplies}, ${pantry_only}, ${language}");
+      "$ingredients, $recipeType, $expSoon, $supplies, $pantryOnly, $language");
 
   var response = await http.post(url, headers: headers, body: requestBody);
-
-  
 
   print('Response status: ${response.statusCode}');
   print('Response body: ${response.body}');
 
   var responseMap = json.decode(response.body);
 
-  /* The Recipe class only has these few fields so we have to hack the recipe data into those existing fields.
-   * Change this when Recipe class gets more complete. */
   return Recipe(ObjectId().toString(), responseMap["recipe_name"],
       selectedItems: ingredients,
-      recipeType: recipe_type,
-      expSoon: exp_soon,
+      recipeType: recipeType,
+      expSoon: expSoon,
       supplies: supplies,
       details: json
           .encode([responseMap["ingredients"], responseMap["instructions"]]));
 }
 
+/// Changes a recipe with the following values
+/// [ingredients] (list of ingredients),
+/// [recipeType] (the type of recipe (eg. vegan)),
+/// [expSoon] (list of must have items in the recipe)
+/// [supplies] (list of kitchen supplies available for use in the recipe),
+/// [pantryOnly] (boolean value whether the recipe only uses items from the pantry or adds new ingredients)
+/// Returns the new modified recipe in a Recipe object
 Future<Recipe> changeRecipe(
     String? details,
     String? change,
     List<String?> ingredients,
-    String? recipe_type,
-    List<String?> exp_soon,
+    String? recipeType,
+    List<String?> expSoon,
     List<String?> supplies,
-    bool? pantry_only) async {
+    bool? pantryOnly) async {
   var url = Uri.https(
       'kitsain-backend-test-ohtuprojekti-staging.apps.ocp-test-0.k8s.it.helsinki.fi',
       '/change'); 
@@ -55,8 +66,8 @@ Future<Recipe> changeRecipe(
   var requestBody = json.encode({
     'change': change,
     'ingredients': ingredients,
-    'recipeType': recipe_type,
-    'expSoon': exp_soon,
+    'recipeType': recipeType,
+    'expSoon': expSoon,
     'supplies': supplies,
     'details': details
   });
